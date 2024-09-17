da3e098ef3aeacd3dd1db66a0c52928abe6bec8f
seo17 <ebenmosi17@gmail.com>
2024-09-17 12:55:53 -0600

    added job model

---
+++ backend/src/db/models/job.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: [true, "userId is required"],
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Job title is required"],
+      trim: true,
+      lowercase: true,
+    },
+    companyName: {
+      type: String,
+      required: [true, "Company Name is required"],
+      trim: true,
+      lowercase: true,
+    },
+    tags: {
+      type: [String],
+      required: false,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    salary: {
+      minimumSalary: { type: Number, required: true },
+      maximumSalary: { type: Number, required: true },
+      currency: { type: String, required: true, trim: true },
+    },
+    location: {
+      country: { type: String, required: true, trim: true },
+      city: { type: String, required: true, trim: true },
+    },
+    jobDescription: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Job", jobSchema);
